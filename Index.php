6282d6d604d416955320652187a46e66d378b6c7
HasanXfatanE <72065442+HasanXfatanE@users.noreply.github.com>
2020-10-23 16:47:29 +0700

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta name="viewport" content="width=device-width,initial-scale=1.0">
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="AboutUs.css">
+	<title>AboutUs</title>
+
+</head>
+<body >
+
+
+	
+		
+	
+
+
+	
+
+
+
+<div id="cursor"></div>
+	<div  class="container" >
+		<div class="card">
+			<div class="img-box">
+				<img src="img\hasan.jpeg">
+			</div>
+				<div class="content">
+			<h2>Hasan</h2>
+			<p>Graphic Design and Editori like play video game and swimming and plaing football</p>
+			<div class="social-media">
+        <ul>
+          <li><a href="#"><img src="images/facebook.png"></a></li>
+           <li><a href="#"><img src="images/whatsapp.png"></a></li>
+           <li><a href="#"><img src="images/line.png"></a></li>
+          <li><a href="#"><img src="images/instagram.png"></a></li>
+        </ul>
+      				</div>
+			</div>
+		</div>
+		<div class="card">
+			<div class="img-box">
+				<img src="img\me.jpg">
+			</div>
+				<div class="content">
+			<h2>Boody </h2>
+			<p>Graphic Design and Editor
+			i like play video game and swimming and plaing football</p>
+			<div class="social-media">
+        <ul>
+          <li><a href="#"><img src="images/facebook.png"></a></li>
+           <li><a href="#"><img src="images/whatsapp.png"></a></li>
+           <li><a href="#"><img src="images/line.png"></a></li>
+          <li><a href="#"><img src="images/instagram.png"></a></li>
+        </ul>
+      				</div>
+			</div>
+		</div>
+		<div class="card">
+			<div class="img-box">
+				<img src="img\Eyad.jpg">
+			</div>
+				<div class="content">
+			<h2>BluEyedo </h2>
+			<p>Graphic Design and Editor i like play video game and swimming and plaing pingpong</p>
+				<div class="social-media">
+        <ul>
+          <li><a href="#"><img src="images/facebook.png"></a></li>
+           <li><a href="#"><img src="images/whatsapp.png"></a></li>
+           <li><a href="#"><img src="images/line.png"></a></li>
+          <li><a href="#"><img src="images/instagram.png"></a></li>
+        </ul>
+      				</div>
+			</div>
+		</div>
+	</div>
+
+	
+<script type="text/javascript">
+	var cursor =  document.getElementById('cursor');
+	document.addEventListener('mousemove', function(e){
+		var x = e.clientX;
+		var y = e.clientY;
+		cursor.style.left = x +"px";
+		cursor.style.top = y +"px";
+
+	});
+</script>
+</body>
+</html>
